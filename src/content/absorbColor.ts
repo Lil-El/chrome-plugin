b0f7c0387926a37cd92a54d42555195142ef8263
yanxd <yxd99324@qq.com>
2025-07-01 10:43:48 +0800

    screenshot page to canvas.

--- src/content/absorbColor.ts
+++ src/content/absorbColor.ts
@@ -1,12 +1,19 @@
+const canvasSize = { width: 240, height: 150 };
+
 main();
 
 async function main() {
-  const overFn = readyFn();
+  const { imageBitmap, canvas: pageCanvas } = await screenshotPage();
 
-  document.addEventListener(
+  const glsCanvas = createGlass(canvasSize.width, canvasSize.height);
+  const glsCtx = glsCanvas.getContext("2d");
+
+  const abortCtrl = new AbortController();
+
+  pageCanvas.addEventListener(
     "click",
     (e) => {
-      overFn();
+      abortCtrl.abort();
 
       const { x, y } = e;
     },
@@ -15,61 +22,75 @@ async function main() {
     }
   );
 
+  pageCanvas.addEventListener(
+    "mousemove",
+    async (e) => {
+      const { x, y } = e;
+
+      glsCanvas.style.left = `${x + 10}px`;
+      glsCanvas.style.top = `${y + 10}px`;
+
+      renderCanvas(pageCanvas, glsCtx, x, y);
+    },
+    {
+      signal: abortCtrl.signal,
+    }
+  );
+}
+
+async function renderCanvas(imageBitmap, context, x, y) {
+  context.clearRect(0, 0, canvasSize.width, canvasSize.height);
+  context.drawImage(
+    imageBitmap,
+    x - canvasSize.width / 20,
+    y - canvasSize.width / 20,
+    canvasSize.width / 10,
+    canvasSize.height / 10,
+    0,
+    0,
+    canvasSize.width,
+    canvasSize.height
+  );
+}
+
+async function screenshotPage() {
   const displayMediaOptions = {
     preferCurrentTab: true,
   };
   const stream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
-  const canvas = document.querySelector("#absorb-color-canvas")! as HTMLCanvasElement;
-  const ctx = canvas.getContext("2d")!;
-
-  const video = document.createElement("video");
-  // video.srcObject = stream;
-  // video.style.width = "100vw";
-  // video.style.height = "100vh";
-  // video.play();
-
-  window.addEventListener("mousemove", async (e) => {
-    const { x, y } = e;
-
-    const canvas = document.querySelector("#absorb-color-canvas")! as HTMLCanvasElement;
-    canvas.style.left = `${x + 10}px`;
-    canvas.style.top = `${y + 10}px`;
-
-    screenshotPage(stream, canvas, ctx, video, x - 160, y - 90);
-  });
-}
-
-function readyFn() {
-  const canvas = createCanvas();
-  document.documentElement.style.pointerEvents = "none";
-  document.documentElement.style.cursor = "crosshair";
-
-  return () => {
-    document.documentElement.style.pointerEvents = "";
-    document.documentElement.style.cursor = "";
-    canvas.remove();
-  };
-}
-
-async function screenshotPage(stream, canvas, ctx, video, x, y) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
   const track = stream.getVideoTracks()[0];
   const imageCapture = new ImageCapture(track);
-  imageCapture.grabFrame().then((imageBitmap) => {
-    console.log(imageBitmap);
+  const imageBitmap = await imageCapture.grabFrame();
 
-    ctx.drawImage(imageBitmap, x, y);
-  });
+  const canvas = createPage(imageBitmap.width, imageBitmap.height);
+  const ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx?.drawImage(imageBitmap, 0, 0);
+
+  return { canvas, imageBitmap };
 }
 
-function createCanvas() {
+function createPage(width, height) {
   const canvas = document.createElement("canvas");
-  canvas.id = "absorb-color-canvas";
+  canvas.style.position = "fixed";
+  canvas.style.left = "0px";
+  canvas.style.top = "0px";
+  canvas.style.width = width + "px";
+  canvas.style.height = height + "px";
+  canvas.style.zIndex = "99999";
+  canvas.style.cursor = "crosshair";
+  document.body.appendChild(canvas);
+  return canvas;
+}
+
+function createGlass(width, height) {
+  const canvas = document.createElement("canvas");
   canvas.style.position = "fixed";
   canvas.style.left = "-1000px";
   canvas.style.top = "-1000px";
-  canvas.style.width = "320px";
-  canvas.style.height = "180px";
+  canvas.style.width = width + "px";
+  canvas.style.height = height + "px";
   canvas.style.border = "1px solid #000";
   canvas.style.pointerEvents = "none";
   canvas.style.zIndex = "999999";
